ccf1a0da422b3831ecd5c45635ac613d1d3229d1
rlwjd31 <rlwjd31@naver.com>
2025-11-06 07:07:26 +0900

    [Feat]: Connect postgres sql db docker image with nestjs

--- learn-nestjs/src/app.module.ts
+++ learn-nestjs/src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    PostsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [],
+      // nestjs에서 작성하는 typeorm 코드와 db의 sync를 자동으로 맞출 것인가에 대한 설정
+      // production환경에서는 마음대로 database의 구조가 바뀔 수 있기 때문에 false를 주어 자동연동을 해지해야한다.
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

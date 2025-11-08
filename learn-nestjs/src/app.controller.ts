eb570111fa0d1944ff009f29618912ff39807336
rlwjd31 <rlwjd31@naver.com>
2025-11-08 21:16:19 +0900

    [Feat]: Implement get, post endpoints to test blogs, tags many-to-many relation

--- learn-nestjs/src/app.controller.ts
+++ learn-nestjs/src/app.controller.ts
@@ -5,6 +5,7 @@ import { UserModel } from 'src/entities/user.entity';
 import { ProfileModel } from 'src/entities/profile.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BlogModel } from 'src/entities/blog.entity';
+import { TagModel } from 'src/entities/tag.entity';
 
 @Controller()
 export class AppController {
@@ -16,6 +17,8 @@ export class AppController {
     private readonly profileRepository: Repository<ProfileModel>,
     @InjectRepository(BlogModel)
     private readonly blogRepository: Repository<BlogModel>,
+    @InjectRepository(TagModel)
+    private readonly tagRepository: Repository<TagModel>,
   ) {}
 
   @Get('users/profile')
@@ -66,5 +69,45 @@ export class AppController {
     });
 
     return newBlog;
+  }
+
+  @Post('blogs/tags')
+  async createBlogsTags() {
+    const blog1 = await this.blogRepository.save({
+      title: 'blog tag test1',
+      content: 'blog tag test1',
+    });
+
+    const blog2 = await this.blogRepository.save({
+      title: 'blog tag test2',
+      content: 'blog tag test2',
+    });
+
+    const tag1 = await this.tagRepository.save({
+      name: 'javascript',
+      blogs: [blog1, blog2],
+    });
+    const tag2 = await this.tagRepository.save({
+      name: 'typescript',
+      blogs: [blog1],
+    });
+
+    const blog3 = await this.blogRepository.save({
+      title: 'Next JS Lecture',
+      content: 'blog tag test3',
+      tags: [tag1, tag2],
+    });
+
+    return true;
+  }
+
+  @Get('blogs')
+  getBlogs() {
+    return this.blogRepository.find({ relations: { tags: true } });
+  }
+
+  @Get('tags')
+  getTags() {
+    return this.tagRepository.find({ relations: { blogs: true } });
   }
 }
